32385515ddcb201ee840efca96071cedd1b5677f
KidneyFlower <753475777@qq.com>
2017-06-25 00:30:51 +0800

    :sparkles: 更新API文档

---
+++ src/views/record/query.vue
@@ -0,0 +1,29 @@
+<template>
+  <c-api-card :data="apiData"></c-api-card>
+</template>
+
+<script>
+  import * as components from 'src/components'
+
+  export default {
+    components,
+    data () {
+      return {
+        apiData: {
+          name: '查询运动记录',
+          method: 'GET',
+          url: '/record/query',
+          params: [
+            {
+              name: 'uid',
+              type: 'String',
+              required: true,
+              description: '用户ID'
+            }
+          ],
+          resDemo: {}
+        }
+      }
+    }
+  }
+</script>
---
+++ src/views/record/rank.vue
@@ -0,0 +1,29 @@
+<template>
+  <c-api-card :data="apiData"></c-api-card>
+</template>
+
+<script>
+  import * as components from 'src/components'
+
+  export default {
+    components,
+    data () {
+      return {
+        apiData: {
+          name: '查询排行榜信息',
+          method: 'GET',
+          url: '/record/rank',
+          params: [
+            {
+              name: 'uid',
+              type: 'String',
+              required: true,
+              description: '用户ID'
+            }
+          ],
+          resDemo: {}
+        }
+      }
+    }
+  }
+</script>
